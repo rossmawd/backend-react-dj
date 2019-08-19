766b14cbb769167806f6742c10ea83881ca8fa62
Ross Mawdsley <rossmawd@gmail.com>
2019-08-19 17:23:30 +0100

    play logic working

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -37,14 +37,15 @@ class Api::V1::ListingsController < ApplicationController
     playlist = Playlist.find(params['listing']['playlist_id'])
     songs = playlist.listings
     sorted_songs = songs.sort { |a, b| a.position <=> b.position }
-    # byebug
+   
     if params['id'] == 'up' && sorted_songs[current_position + 1]
       other_listing = sorted_songs.filter { |listing| listing.position == current_position + 1 }.last
 
       Listing.update(other_listing.id, position: current_position)
       Listing.update(listing_id, position: current_position + 1)
+      updated_playlist = Playlist.find(playlist.id)
 
-      render json: { messsage: 'Moved Up!' }, status: :created
+      render json: updated_playlist, status: :created
     # check for valid move (cannot go above the top, or below bottom)
 
     elsif params['id'] == 'down' && ((current_position - 1) >= 0)
@@ -53,23 +54,25 @@ class Api::V1::ListingsController < ApplicationController
 
       Listing.update(other_listing.id, position: current_position)
       Listing.update(listing_id, position: current_position - 1)
+      updated_playlist = Playlist.find(playlist.id)
 
-      render json: { messsage: 'Moved Down!' }, status: :created
+      render json: updated_playlist, status: :created
 
     elsif params['id'] == 'delete' 
        
       top_index=(sorted_songs.length)-1 
       number_to_update = top_index - (current_position )
       x = current_position + 1
-      number_to_update.times do
-        
-        to_update = sorted_songs.filter  { |listing| listing.position == x }.last
-   
 
+      number_to_update.times do  
+        to_update = sorted_songs.filter  { |listing| listing.position == x }.last
         Listing.update(to_update.id, position: x-1)
         x += 1
       end
-      render json: { messsage: 'All higher positions changed!' }, status: :created
+      
+      updated_playlist = Playlist.find(playlist.id)
+
+      render json: updated_playlist, status: :created
 
     else
       render json: { error: "Can't be moved any further!" }
